53559c4a908dc2e1504c2abfba7a9b0707eaf201
sak <sak255@gmail.com>
2017-11-09 23:52:22 +0900

    Use : comment instead of rem.

--- install.bat
+++ install.bat
@@ -1,18 +1,20 @@
+: bat
+
 set dotfiles=%~dp0
 
-@rem Link git settings.
+: Link git settings.
 del %home%\.gitconfig
 mklink %home%\.gitconfig %dotfiles%\.gitconfig
 mkdir %home%\.config\git
 del %home%\.config\git\ignore
 mklink %home%\.config\git\ignore %dotfiles%\.config\git\ignore
 
-@rem Link atom settings.
+: Link atom settings.
 mkdir %home%\.atom
 del %home%\.atom\config.cson
 mklink %home%\.atom\config.cson %dotfiles%\.atom\config.cson
 del %home%\.atom\keymap.cson
 mklink %home%\.atom\keymap.cson %dotfiles%\.atom\keymap.cson
 
-@rem Install atom packages.
+: Install atom packages.
 apm install --packages-file %dotfiles%\.atom\packages.txt
